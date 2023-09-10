839332ed62a605b43b75fe30bcd8a507c8ba3dfc
yinq <>
2023-09-10 23:43:47 +0800

    0000

---
+++ src/main/java/com/exercise/leet/array/Solution0000.java
@@ -0,0 +1,13 @@
+package com.exercise.leet.array;
+
+import com.alibaba.fastjson.JSON;
+
+public class Solution0000 {
+
+    public static void main(String[] args) {
+        Solution0000 obj = new Solution0000();
+        System.out.println(obj);
+    }
+
+
+}
